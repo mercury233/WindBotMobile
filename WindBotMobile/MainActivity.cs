977f0bf6f7e3a3e80b1908ff132010072147ee61
mercury233 <me@mercury233.me>
2018-05-09 21:36:15 +0800

    test read cdb

--- WindBotMobile/MainActivity.cs
+++ WindBotMobile/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
@@ -14,6 +15,29 @@ namespace WindBotMobile
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += Test1;
+
+           // try
+            //{
+                YGOSharp.OCGWrapper.NamedCardsManager.Init("/storage/emulated/0/ygocore/cards.cdb");
+
+            //}
+           // catch (Exception e)
+            //{
+           //     EditText textview = FindViewById<EditText>(Resource.Id.editText1);
+            //    textview.Text = e.Message;
+           // }
+        }
+
+        private void Test1(object sender, EventArgs e)
+        {
+            EditText textview = FindViewById<EditText>(Resource.Id.editText1);
+            YGOSharp.OCGWrapper.NamedCardsManager.Init("/storage/emulated/0/ygocore/cards.cdb");
+            YGOSharp.OCGWrapper.NamedCard card1 = YGOSharp.OCGWrapper.NamedCard.Get(22862454);
+
+            textview.Text = card1.Name;
         }
     }
 }
